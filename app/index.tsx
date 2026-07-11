0b4acb882034630dd84ef8a5671d97609ca1533c
Alejndro-Elias <alejandroelias25@gmail.com>
2026-07-11 20:10:43 -0300

    Agrega mapa al proyecto

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,56 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import MapView, { UrlTile } from "react-native-maps";
+import * as Location from "expo-location";
+import { useEffect, useRef, useState } from "react";
 
 export default function Index() {
+  const [location, setLocation] =
+    useState<Location.LocationObjectCoords | null>(null);
+  const mapRef = useRef<MapView>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        console.log("Permiso denegado");
+        return;
+      }
+
+      const currentLocation = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.High,
+      });
+
+      mapRef.current?.animateToRegion(
+        {
+          latitude: currentLocation.coords.latitude,
+          longitude: currentLocation.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        },
+        1000,
+      );
+
+      setLocation(currentLocation.coords);
+    })();
+  }, []);
+
+  if (location) {
+    console.log(location.latitude);
+  }
+
   return (
-    <View>
-      <Text className="text-orange-800" >Edit app/index.tsx to edit this screen.</Text>
+    <View className="h-2/6">
+      <MapView
+        ref={mapRef}
+        style={{ flex: 1 }}
+        showsUserLocation
+      >
+        <UrlTile
+          urlTemplate="https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=LU8YiM3OajbLLB8UgfCo"
+          maximumZ={20}
+        />
+      </MapView>
     </View>
   );
 }
